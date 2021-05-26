1054256f45b704e5287e9e81b1e374a0a1e03c99
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-05-26 19:18:11 -0300

    Create main layout content
    - Add a prototype of how the santander app look like.
    - Add new drawables for icons
    - add content_main.xml for segregation of the content view in the activity_main.xml
    - still needs more refactoring in content_main.xml

--- app/src/main/java/com/miguelsantos/notsantander/MainActivity.kt
+++ app/src/main/java/com/miguelsantos/notsantander/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.miguelsantos.notsantander
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,5 +18,21 @@ class MainActivity : AppCompatActivity() {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setHomeAsUpIndicator(R.drawable.reorder_white_24dp)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_notification -> {
+                Toast.makeText(this, "Olha a notificação dindin", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
