916f7c17b4ee2ac4dc9c095abbd0f32d02831285
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-05-28 16:03:44 -0300

    Add Firebase Extensions
    - Now the application has Firebase Analytics, Crashlytics and Runtime Deployment implemented
    - Removed deprecated kotlin-android-extensions from the build.gradle as well as every use of it in the code.
    - Add new strings and text format for the variables of the text.

--- app/src/main/java/com/miguelsantos/notsantander/ui/MainActivity.kt
+++ app/src/main/java/com/miguelsantos/notsantander/ui/MainActivity.kt
@@ -3,14 +3,13 @@ package com.miguelsantos.notsantander.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.miguelsantos.notsantander.R
 import com.miguelsantos.notsantander.data.Account
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(main_toolbar)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
         if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setHomeAsUpIndicator(R.drawable.reorder_white_24dp)
@@ -40,17 +39,26 @@ class MainActivity : AppCompatActivity() {
     private fun onBindView(account: Account) {
         // Pega o nome completo e divide pelos espaços.
         val clientName = account.client.name.split(" ")
-        main_tv_username.text = resources.getString(R.string.hello, clientName[0])
+        findViewById<TextView>(R.id.main_tv_username).text =
+            String.format(resources.getString(R.string.hello), clientName[0])
 
-        main_tv_agency.text = "Ag ${account.agency}"
-        main_tv_account_num.text = "Cc ${account.accountNumber}"
-        main_tv_available_balance.text = "R$ ${account.balance}"
-        main_tv_balance_plus_limit.text = "R$ ${account.limit}"
+        findViewById<TextView>(R.id.main_tv_agency).text =
+            String.format(resources.getString(R.string.agency), account.agency)
+
+        findViewById<TextView>(R.id.main_tv_account_num).text =
+            String.format(resources.getString(R.string.account), account.accountNumber)
+
+        findViewById<TextView>(R.id.main_tv_available_balance).text =
+            String.format(resources.getString(R.string.available_balance_cash), account.balance)
+
+        findViewById<TextView>(R.id.main_tv_balance_plus_limit).text =
+            String.format(resources.getString(R.string.balance_limit_cash), account.limit)
 
         //Pegando os 4 últimos números do cartão
         val card = account.card.cardNumber
-        main_tv_card_final_nums.text =
-            card.substring(card.length - 5)
+        findViewById<TextView>(R.id.main_tv_card_final_nums).text = String.format(
+            resources.getString(R.string.final_card_numbers), card.substring(card.length - 5)
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -61,6 +69,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_notification -> {
+
                 Toast.makeText(this, "Olha a notificação dindin", Toast.LENGTH_LONG).show()
                 true
             }
